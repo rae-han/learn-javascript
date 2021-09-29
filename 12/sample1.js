ea258b260deda2fb938dba2daf11eb21322dfd5a
Raehan <raehan900809@gmail.com>
2021-09-29 20:17:20 +0900

    Add function content

---
+++ 12/sample1.js
@@ -0,0 +1,12 @@
+let f = function add(a, b) {
+  return a+b;
+}
+
+console.log(f(1, 2));
+// console.log(add(1, 2));
+
+function multiply(x, y) {
+  console.log(x * y);
+}
+
+console.log(multiply(1, 2))
