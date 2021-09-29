2d428b302e1e0114aa9404472d89bf700dbe7fa3
Raehan <raehan900809@gmail.com>
2021-09-29 12:04:56 +0900

    Add yesterday

---
+++ 7/sample1.js
@@ -0,0 +1,11 @@
+let result, num = 1;
+let numA = 1;
+let numB = 1;
+
+console.log(numA++);
+console.log(++numB);
+
+result = num++;
+
+console.log(result, num)
+
---
+++ 8/sample1.js
@@ -0,0 +1,8 @@
+{
+  let variable = 'check';
+  console.log(variable)
+  console.log(1)
+}
+
+console.log(typeof String(Infinity));
+console.log(typeof Infinity.toString());
