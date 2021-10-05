6249194800d51dcb41206adeb5c39cfcf64dbc72
Raehan <raehan900809@gmail.com>
2021-10-05 11:19:02 +0900

    Add example code

--- deepdive/24/0.js
+++ deepdive/24/0.js
@@ -1,11 +1,3 @@
-# 클로저
-
-## 클로저의 정의
-
-> 클로저는 함수와 그 함수가 선언된 렉시컬 환경과의 조합이다.   
-A closure is the combination of a function and the lexical environment within which that function was declared.
-
-```javacript
 const x = 1;
 
 function outerFunc1() {
@@ -45,4 +37,3 @@ const outerFunc3 = () => {
 }
 
 outerFunc3();
-```
