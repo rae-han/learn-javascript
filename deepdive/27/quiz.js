78942dd46940f8932ae9d75827aaa95f947d8b57
Raehan <raehan900809@gmail.com>
2021-10-28 14:19:04 +0900

    Add quiz

--- deepdive/27/quiz.js
+++ deepdive/27/quiz.js
@@ -2,17 +2,24 @@ const func = (value = '') => {
   value = '' + value
 
   return value.split('').reverse().reduce((acc, digit, idx) => {
-    if (idx > 0 && idx % 3 === 0) acc.push(',')
+    if (idx > 0 && idx % 3 === 0 && digit !== '-') acc.push(',')
     return [...acc, digit]
   }, []).reverse().join('')
 }
 
-console.log(func(1234567890))
-console.log(func(-1234567890))
-console.log(func(-123456789))
+console.log(func(1234567890));
+console.log(func(-1234567890));
+console.log(func(-123456789));
 
-function sum(a, b, c=0, d) {
-  console.log(a,b,c,d);
-}
+const cities = {
+  London: 'UK',
+  Manchester: 'UK',
+  Liverpool: 'UK',
+  Paris: 'France',
+  Berlin: 'Germany',
+  Munchen: 'Germany',
+};
 
-console.log(Object.getOwnPropertyDescriptors(sum));
+let countries = Object.entries(cities).reduce((acc, [city, country]) => ({ ...acc, [country]: [...(acc[country] || []), city] }), {});
+
+// console.log(countries);
--- deepdive/27/sample4.js
+++ deepdive/27/sample4.js
@@ -12,10 +12,33 @@
 
 // const numbers = new Numbers();
 // numbers.multiply([1, 2, 3]);
+/*
+let testCases = [100000, 1000000, 1200000, 8000000];
 
-// let testcase = 8000000;
+let testResults = testCases.forEach(testcase => {
+  let testArray = Array.from(new Array(testcase), (_, i) => ({ key: i}))
+
+  console.log(`\n#### testcase: ${testcase} ####`)
+
+  console.time("for of time");
+  for(const item of testArray) { 
+    // return item;
+  }
+  console.timeEnd("for of time");
+
+  console.time("forEach time");
+  testArray.forEach(item => { })
+  console.timeEnd("forEach time");
+
+  console.time("for time");
+  for(let i=0; i<testArray.length; i++) { }
+  console.timeEnd("for time");
+});
+
+console.log(`\n#### testcase ####`)
+let testcase = 8000000;
 // let testcase = 1200000;
-let testcase = 1000000;
+// let testcase = 1000000;
 // let testcase = 100000;
 
 let testArr = Array.from(new Array(testcase), (_, i) => ({ key: i}))
@@ -33,3 +56,32 @@ console.timeEnd("forEach time");
 console.time("for time");
 for(let i=0; i<testArr.length; i++) { }
 console.timeEnd("for time");
+*/
+
+let arr = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5];
+
+console.log(arr.filter(() => false));
+
+let sum1 = arr.reduce((acc, cur) => acc + cur, 0);
+console.log(sum1);
+
+let sum2 = arr.reduce((acc, cur) => acc + cur);
+console.log(sum2)
+
+const average = arr.reduce((acc, cur, i, { length }) => {
+  console.log(acc, length)
+  return i === length-1 ? (acc+cur)/length : acc+cur
+}, 0);
+
+console.log(average);
+
+const max = arr.reduce((acc, cur) => acc>cur ? acc : cur, 0);
+console.log(max);
+
+const count = arr.reduce((acc, cur) => {
+  acc[cur] = (acc[cur] || 0) + 1;
+  return acc;
+}, {});
+
+console.log(count);
+
