1effbcd7dd61efa0126d0b3ed467777475621fc0
Raehan <raehan900809@gmail.com>
2021-11-01 11:45:06 +0900

    Add ex number

--- deepdive/28/sample.js
+++ deepdive/28/sample.js
@@ -1,2 +1,13 @@
 let numObj = new Number(1);
+let numInstance = Number(1);
 console.log(numObj === 1);
+console.log(numObj);
+console.log(numObj.Number === 1);
+console.log(numInstance === 1);
+
+console.log(typeof 12e+1);
+console.log(typeof 12e1);
+console.log(typeof 12e-1);
+console.log(typeof 0b010);
+console.log(typeof 0o123);
+console.log(typeof 0x123);
