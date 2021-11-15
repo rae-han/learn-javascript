2298d47c878ea359037be0635099476a715b4d08
Raehan <raehan900809@gmail.com>
2021-11-15 16:14:55 +0900

    Add

---
+++ deepdive/35/11.js
@@ -0,0 +1,6 @@
+let arr1 = [1, 4];
+let arr2 = [2, 3];
+
+console.log(arr1.splice(1, 0, arr2));
+console.log(arr1);
+
---
+++ deepdive/35/16.js
@@ -0,0 +1,4 @@
+let arr = [1, 2, 3, 4];
+
+const copyArr = Array.prototype.slice.call(arr);
+console.log(copyArr)
