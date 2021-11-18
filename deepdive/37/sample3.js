963f72991c09a18a69104d38e675371236a670ce
Raehan <raehan900809@gmail.com>
2021-11-18 19:22:32 +0900

    Add sample source

---
+++ deepdive/37/sample3.js
@@ -0,0 +1,16 @@
+// const map1 = new Map();
+// const map2 = new Map();
+
+// console.log(map1)
+// console.log(map2)
+
+// const map = new Object(1, 2);
+// console.log(obj)
+
+const map = new Map();
+const set = new Set();
+
+console.log(map instanceof Map)
+console.log(map instanceof Object)
+console.log(set instanceof Set)
+console.log(set instanceof Object)
