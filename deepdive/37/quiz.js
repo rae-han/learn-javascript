326487241de70d7ea5421fef80dd338c5ac46a0a
Raehan <raehan900809@gmail.com>
2021-11-18 18:35:51 +0900

    Modify quiz

--- deepdive/37/quiz.js
+++ deepdive/37/quiz.js
@@ -1,12 +1,11 @@
 let map = new Map();
 map.set('name', 'raehan');
 
-let keys = map.keys();
-console.log(keys);
+let values = map.values();
+console.log(values)
+values.push('newValue');
 
-keys.push("more value");
-
-console.log(keys);
+console.log(values);
 
 // 어떻게 동작 할까?
 // 문제가 있다면 왜 그렇고 어떤 부분을 수정해야 할까??
