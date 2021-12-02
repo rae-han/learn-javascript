3439b0415722d5c192c1a3fe4afa86df97d5314e
Raehan <raehan900809@gmail.com>
2021-12-02 19:46:17 +0900

    Add callback function

--- deepdive/42/test.js
+++ deepdive/42/test.js
@@ -1,12 +1,15 @@
 console.log('start');
 
-const logFunc = (text) => console.log(text)
+const logFunc = (text) => console.log(text);
 
-setTimeout(() => logFunc('callback'), 0);
+setTimeout(() => logFunc('callback 1'), 1000);
+setTimeout(() => logFunc('callback 2'), 0);
 
 const _DELAY = 1000000001;
-for(let i = 0; i<_DELAY; i++) { if(i === (_DELAY-1)) console.log('delay') };
+for(let i = 0; i<_DELAY; i++) { if(i === (_DELAY-1)) console.log('delay') }
 
-logFunc('global')
+logFunc('global');
 
 console.log('end');
+
+// http://latentflip.com/loupe
