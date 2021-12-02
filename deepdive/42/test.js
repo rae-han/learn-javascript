a892b79901af5211dcd54cf6b0d975bc48e25070
Raehan <raehan900809@gmail.com>
2021-12-02 19:12:13 +0900

    Modify convention

--- deepdive/42/test.js
+++ deepdive/42/test.js
@@ -4,8 +4,8 @@ const logFunc = (text) => console.log(text)
 
 setTimeout(() => logFunc('callback'), 0);
 
-const delay = 1000000001;
-for(let i = 0; i<delay; i++) { if(i === (delay-1)) console.log(i) };
+const _DELAY = 1000000001;
+for(let i = 0; i<_DELAY; i++) { if(i === (_DELAY-1)) console.log('delay') };
 
 logFunc('global')
 
