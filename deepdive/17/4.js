7a91f5c1088bb6d101bffb855b9ec2051afe0286
raehan <raehan900809@gmail.com>
2021-12-31 23:05:06 +0900

    add: new function

--- deepdive/17/4.js
+++ deepdive/17/4.js
@@ -7,10 +7,17 @@ function Circle(radius = 1) {
 
 const circle1 = new Circle(5);
 const circle2 = new Circle(10);
+const circle3 = new Circle(10);
+
+circle3.getDiameter = function() {
+  return 3*this.radius;
+}
 
 console.log(circle1.getDiameter());
 console.log(circle2.getDiameter());
+console.log(circle3.getDiameter());
+console.log(circle1.getDiameter === circle2.getDiameter);
 
 // console.log(radius);
-const circle3 = Circle(15);
+const circle4 = Circle(15);
 console.log(radius);
