f02a054fce515d4301f0dd67c50976419e938fa2
raehan <raehan900809@gmail.com>
2022-01-23 21:57:03 +0900

    add: websocket

--- apple-coding/web-socket/server.js
+++ apple-coding/web-socket/server.js
@@ -12,3 +12,12 @@ const WebSocket = require('ws');
 const socket = new WebSocket.Server({
   port: 8081
 });
+
+socket.on('connection', (ws, req) => {
+  ws.on('message', (msg) => {
+    console.log('유저가 보낸거 : ' + msg);
+
+    // 서버가 유저한테 보낼 때
+    ws.send('bye');
+  })
+})
