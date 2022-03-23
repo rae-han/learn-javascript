887f43870429dc5c7886232ecc1aff088ae6953f
Raehan <raehan900809@gmail.com>
2022-03-23 19:38:36 +0900

    add: function pipe, go

--- functional-javascript/es5-1st/introduction/_.js
+++ functional-javascript/es5-1st/introduction/_.js
@@ -1,229 +1,106 @@
-// const _each = (list, iter) => {
-//   for(let i = 0; i < list.length; i++) {
-//     iter(list[i]); // 반복문을 돌면서 안에서 하는 일을 위임하는 함수
-//   }
+let users = [
+  { id: 1, name: 'ID', age: 36 },
+  { id: 2, name: 'BJ', age: 32 },
+  { id: 3, name: 'JM', age: 32 },
+  { id: 4, name: 'PJ', age: 27 },
+  { id: 5, name: 'HA', age: 25 },
+  { id: 6, name: 'JE', age: 26 },
+  { id: 7, name: 'JI', age: 31 },
+  { id: 8, name: 'MP', age: 23 }
+];
 
-//   return list;
-// }
-
-function _each(list, iter) {
-  for(let i = 0; i < list.length; i++) {
-    iter(list[i]); // 반복문을 돌면서 안에서 하는 일을 위임하는 함수
-  }
-
-  return list;
+const _each = (list, iteratee) => {
+	for(let i = 0; i < list.length; i++) {
+		iteratee(list[i]);
+	}
 }
 
-// const _filter = (list, predi) => {
-//   let new_list = [];
-
-//   for(let i = 0; i<list.length; i++) {
-//     // 함수형은 중복을 제거하거나 추상화할 때 함수를 사용한다
-//     // 추상화의 단위가 객체, 클래스, 메소드가 아니라 함수를 이용하는 것
-//     if(predi(list[i])) { 
-//       // 어떤 조건일때 if문 안으로 들어올지를 predi 함수에게 위임한다
-//       // 이렇게 함수를 인자로 받아서 원하는 시점에 해당하는 함수가 알고있는 인자를 적용하는 것이 응용형 프로그래밍, 응용형 함수, 적용형 프로그래밍이라 한다.
-//       // 함수를 인자로 받거나, 받은 함수를 실행하거나, 함수를 리턴하는 걸 고차함수라 한다.
-//       new_list.push(list[i]);
-
+const _curryr = fn => (a, b) => b !== undefined ? fn(a, b) : (b => fn(b, a));
+// function _curryr(fn) {
+//   return function(a, b) {
+//     return arguments.length === 2 ? fn(a, b) : function(b) {
+//       return fn(b, a); // 미리 받은 함수 본체를 안쪽에서 평가
 //     }
 //   }
-
-//   return new_list; // console.log 는 외부에 영향을 준다?
 // }
-function _filter(list, predi) {
-  let new_list = [];
+// const _curryr = fn => {
+//   return (a, b) => {
+//     console.log(1111)
+//     console.log(a, b)
+//     return b !== undefined ? fn(a, b) : b => fn(b, a)
+//   }
+// }
 
-  _each(list, function(val) {
-    if(predi(val)) {
-      new_list.push(val)
-    }
-  });
+let _filter = (list, predicate) => {
+	let new_list = [];
 
-  return new_list; // 이거 주석 처리하면 이상한 에러가?
+	_each(list, value => {
+		if(predicate(value)) {
+			new_list.push(value)
+		}
+	});
+
+	return new_list;
 }
-
-// const _map = (list, mapper) => {
+// function _filter(list, predi) {
 //   let new_list = [];
+//   console.log('predi', predi)
 
-//   // for(let i = 0; i < list.length; i++) {
-//   //   new_list.push(mapper(list[i]));
-//   // }
-//   // /**
-//   //  * mapper 함수에게 어떤 데이터를 수집할지 완전히 위임한다
-//   //  * 데이터형이 어떻게 생겼는지 보이지 않는다 -> 함수형 프로그래밍의 중요한 특징중 하나
-//   //  * 관심사가 완전비 불리된다
-//   //  */
+//   _each(list, function(val) {
+//     if(predi(val)) {
+//       new_list.push(val)
+//     }
+//   });
 
-//   _each(list, val => new_list.push(mapper(list[val])));
-
-//   return new_list;
+//   return new_list; // 이거 주석 처리하면 이상한 에러가?
 // }
-function _map(list, mapper) {
+_filter = _curryr(_filter);
+
+let _map = (list, mapper) => {
   let new_list = [];
 
-  _each(list, function(val) {
-    new_list.push(mapper(val));
-  })
+  _each(list, value => {
+		new_list.push(mapper(value));
+	})
 
   return new_list;
 }
+// function _map(list, mapper) {
+//   let new_list = [];
 
-/**
- * # 다형성
- * 자바스크립트에는 이미 Array 객체에 map, filter 함수가 있다
- * 하지만 우리가 이걸 만든 이유는 저건 함수가 아니라 메서드이다
- * 순수 함수가 아니라 객체의 상태에 따라 결과가 달라진다
- * 
- * 메서드는 객체지향 프로그래밍
- * 
- * 메서드는 해당 클래스에 정의돼 있기 때문에 해당 클래스의 인스턴스에만 사용 가능하다
- * 위의 함수들도 Array 에서만 사용 가능하다
- * 
- * 자바스크립트는 Array가 아닌다 Array같이 여겨지는 객체가 많다(유사배열)
- * 
- * 함수형은 함수에 맞게 데이터만 맞추면 데이터에 영향 받지 않고 사용 가능하다
- * 
- * 다형성을 높게 프로그래밍을 할수 있다
- * 데이터가 먼저 나오는 프로그래밍보다 함수가 먼저 나오는 프로그래밍이
- * 
- * 객체 지향은 해당하는 객체가 생겨야 기능을 수행할수 있다
- * 
- * 함수는 함수가 먼저 존재하기 때문에, 데이터가 생기지 않더라도 평가 시점이 상대적으로 유연해진다
- * 이 이유 덕분에 더 높은 조합성을 갖는다
- */
+//   _each(list, function(val) {
+//     new_list.push(mapper(val));
+//   })
 
-_map([1, 2, 3, 4], function(v) { return v })
-/**
- * 여기서 두 번째 인자는 반드시 콜백 함수가 아니다
- * 무조건 콜백 함수라는 명칭으로 부를때가 있는데 프레디케이트나 이터 맴퍼 등의 정확한 명칭을 쓴느게 좋다
- */
+//   return new_list;
+// }
+_map = _curryr(_map);
 
-/**
- * 2. 외부 다형성
- * - array_like 값이 들어오거나 arguments 값이 들어오거나, document.querySelectorAll 값이 들어오거나 다 실행 가능
- * - 돌림직한 모든 객체를 다 돌릴수 있또록 하는 것은 고차함수의 구조 맵이 어떻게 구현됐느냐에 따라 결정되지만
- * - 순수 함수로 만들었고 어레이라이ㅡ면 다 돌릴수 있게 만든 기법은 맵 이치 필터가 담당하지만
- * 
- * 3. 내부 다형성
- * - 배열안에 어떤 값이든 들어있어도 다 수행할수 있게 하는 것은 2번째 인자인 보조 함수가 하는 역할이다
- * - 프레디 맵퍼 이터레이터
- * - 내부 값에 대한 다형성은 보조함수가 책임을 지기 때문에
- * - 넘기는 값과 그 값에 대한 이해로 숫자니까 더할 것이라던지 노드니까 노드 네임을 참조할 것이라던지 개발자가 정할 수 있기 때문에 
- * - 보조 함수에 위임하기 때문에 데이터 형으로부터 자유롭고 다형성을 높이는데 유리하다
- */
-
-/**
- * 4. 커링 ( _curry, _curryr)
- */
-
-function _curry(fn) {
-  return function(a) {
-    return function(b) {
-      return fn(a, b); // 미리 받은 함수 본체를 안쪽에서 평가
-    }
-  }
+const _log_length = value => {
+	return value;
 }
 
-const add1 = function(a, b) {
-  return a+b;
-}
+const _curry = (fn) => (a, b) => arguments.length === 2 ? fn(a, b) : b => fn(a, b);
+// function _curry(fn) {
+//   return function(a) {
+//     return function(b) {
+//       return fn(a, b); // 미리 받은 함수 본체를 안쪽에서 평가
+//     }
+//   }
+// }
+// const _curryr = fn => (a, b) => arguments.length === 2 ? fn(a, b) : b => fn(b, a);
 
-console.log(add1(5, 10));
+// let _get = (obj, key) => obj === null ? undefined : obj[key];
+const _get = _curryr((obj, key) => obj === null ? undefined : obj[key]);;
 
-const add2 = _curry(function(a, b) {
-  return a+b;
-})
+// const _reduce = (list, iteratee, memo) => {
+// 	_each(list, value => {
+// 		memo = iteratee(memo, value)
+// 	})
 
-const add5 = add2(5);
-console.log(add5(10));
-console.log(add2(10)(5));
-
-function _curry2(fn) {
-  return function(a, b) {
-    if(arguments.length === 2) return fn(a, b);
-
-    return function(b) {
-      return fn(a, b); // 미리 받은 함수 본체를 안쪽에서 평가
-    }
-  }
-}
-
-const add3 = _curry2(function(a, b) {
-  return a+b;
-})
-
-console.log(add3(10)(5))
-console.log(add3(10, 5))
-
-function _curry3(fn) {
-  return function(a, b) {
-    return arguments.length === 2 ? fn(a, b) : function(b) {
-      return fn(a, b); // 미리 받은 함수 본체를 안쪽에서 평가
-    }
-  }
-}
-
-const add4 = _curry3(function(a, b) {
-  return a+b;
-})
-
-console.log(add4(10)(5))
-console.log(add4(10, 5))
-
-let sub1 = _curry3(function(a, b) {
-  return a - b;
-})
-
-console.log(sub1(10, 5))
-
-let sub10 = sub1(10);
-console.log(sub10(5)) 
-// 10 에 5를 빼는건데 5에서 10일 빼는 느낌의 함수 네임
-// 이때 쓰는게 curryr(curry right)
-
-function _curryr1(fn) {
-  return function(a, b) {
-    return arguments.length === 2 ? fn(a, b) : function(b) {
-      return fn(b, a); // 미리 받은 함수 본체를 안쪽에서 평가
-    }
-  }
-}
-
-let sub2 = _curryr1(function(a, b) {
-  return a - b;
-})
-
-let sub10_2 = sub2(10);
-console.log(sub10_2(5));
-
-/**
- * _get
- */
-
-function _get1(obj, key) {
-  return obj == null ? undefined : obj[key];
-}
-
-let user1 = { id: 1, name: 'NM', age: 31 };
-console.log(user1.name);
-console.log(_get1(user1, 'name'));
-// user1 값이 없다면 첫 번째는 에러, 두 번째는 undefined를 뱉는다
-
-let _get2 = _curryr1(function(obj, key) {
-  return obj === null ? undefined : obj[key];
-});
-/**
- * _get 함수를 정의 할때 curryr를 통해 함수를 정의하게 되면 평가 순서를 커링을 통해 뒤짚어 인자를 오른쪽부터 적용할수 있게 사용할수 있게 된다
- * _get2(user1, 'name') 보다 _get2('name', user1)이 더 직관적이다
- */
-
-let get_name = _get2('name');
-console.log(get_name(user1)) // 이걸로 sample2.js // 001 수정
-
-const _reduce = (...args) => {
-  let [list, iter, memo] = args;
-  // console.log(list, iter, memo)
+// 	return memo;
+// }
+const _reduce = (list, iter, memo) => {
   // 003
   if(memo == undefined) {
     console.log(list)
@@ -242,14 +119,22 @@ const _reduce = (...args) => {
   return memo;
 }
 
-console.clear();
+let slice = Array.prototype.slice;
+function _rest(list, num) {
+  return slice.call(list, num || 1)
+}
 
 module.exports = {
+  users,
   _filter,
   _map,
   _each,
-  _get: _get2,
+  _get,
   _reduce,
+  _rest,
+  _log_length,
+  _curry,
+  _curryr,
 }
 
 // 여기서부터 그냥 002로 이동
--- functional-javascript/es5-1st/introduction/sample3.js
+++ functional-javascript/es5-1st/introduction/sample3.js
@@ -1,4 +1,4 @@
-const { _reduce } = require('./_')
+let { _reduce, _map, _rest, _filter, _get, _log_length, users, _curryr } = require('./_')
 
 // _pipe
 // 함수를 인자로 받아서 함수를 연속적으로 실행시켜주는 함수.
@@ -18,11 +18,198 @@ const { _reduce } = require('./_')
 //   }
 // }
 
-const _pipe = (...fns) => arg => _reduce(fns, (arg, fn) => fn(arg), arg)
+const _pipe = (...fns) => arg => _reduce(fns, (arg, fn) => fn(arg), arg);
 
 const fn = _pipe(
   n => n + 1,
-  n => n * 2
+  n => n * 2,
+  n => n * n,
 );
 
 console.log(fn(1));
+
+// _go
+// 즉시 실행되는 pipe 함수
+// pipe 는 함수를 리턴하는 함수
+// go는 첫번째 값을 인자로 두번째 값부터 함수를 받아서 즉시 실행시키는 함수
+
+
+// function _go(arg) {
+//   let fns = _rest(arguments);
+//   // pipeline을 만들 때,
+//   // pipeline은 인자 들로 함수를 받기 때문에 aplly를 통해 인자를 받는다.
+//   return _pipe.apply(null, fns)(arg);
+// }
+
+const _go = (arg, ...fns) => _pipe.apply(null, fns)(arg);
+
+_go(
+  1,
+  n => n + 1,
+  n => n * 2,
+  n => n * n,
+  console.log,
+)
+
+// 1. 2. 30세 이상인 users의 names를 수집한다.
+console.log(_log_length(
+	_map(
+		_filter(users, user => user.age >= 30), 
+		_get('name')
+	),
+));
+
+// 3. 4. 30세 미만인 users의 ages를 수집한다.
+console.log(_log_length(
+	_map(
+		_filter(users, user => user.age < 30), 
+		_get('age')
+	),
+));
+
+// 기존 함수는 함수 안쪽부터 시작해서 밖으로 전개가 되니 사람이 읽기가 불편한 점이 있다.
+
+_go(
+  users,
+  function(users) {
+    return _filter(users, function(user) {
+      return user.age >= 30;
+    });
+  },
+  function(users) {
+    return _map(users, _get('name'));
+  },
+  console.log,
+)
+
+_go(
+  users,
+  function(users) {
+    return _filter(users, function(user) {
+      return user.age < 30;
+    });
+  },
+  function(users) {
+    return _map(users, _get('age'));
+  },
+  console.log,
+)
+
+// 위 함수 패턴을 보면 users를 받아서 안쪽 함수에서 다시 user를 받고 그 안에서 데이터를 리턴하고
+// 안쪽 함수는 항상 보조 함수가 있는 형태이다.
+// curryr을 사용하면 간결하게 바꿀 수 있다.
+
+// _map, filter에 curryr을 적용하면?
+_map = _curryr(_map);
+_filter = _curryr(_filter);
+
+const numbers = [1, 2, 3, 4];
+const callbackFunc = num => num*num;
+
+console.log(
+  _map(numbers, callbackFunc),
+  _map(callbackFunc)(numbers), 
+  // 2개 모두 같은 값을 가리킨다
+)
+
+// 평가 순서를 다르게 갈 수 있다.
+// 그렇다면 위에 코드를 바꿀 수 있다.
+// 훨씬 갈결하고 체인패턴(메소드 체이닝)과 비슷한 정도의 표현력을 보여준다.
+_go(
+  users,
+  _filter(function(user) {
+      return user.age >= 30;
+  }),
+  _map(_get('name')),
+  console.log,
+)
+
+_go(
+  users,
+  _filter(function(user) {
+      return user.age < 30;
+  }),
+  _map(_get('age')),
+  console.log,
+)
+
+_go(
+  users,
+  _filter((user) => user.age >= 30),
+  _map(_get('name')),
+  console.log,
+)
+
+_go(
+  users,
+  _filter((user) => user.age < 30),
+  _map(_get('age')),
+  console.log,
+)
+
+// _pipe와 _go 함수를 사용하면 기존의 함수를 중첩 하는 것보다 훨씬 명시적으로 코드를 읽어 나갈수 있다.
+
+// 커리와 파이프 함수가 필터 맵 함수를 만들때 커리알을 이용해서 만들었고
+// 그로 인해 그 함수가 인자를 하나, 함수 하나만 넘겼을 때 인자가 오른쪽부터 적용된 또 다른 함수를 리턴하게 되고
+// 고라는 함수로 인자와 함수들을 받아서 함수를 연속적으로 실행 시키면서 인자를 변화시키면서 함수형 프로그래밍
+// 함수의 평가시점이나 함수가 인자에 적용 되 가는 과정에서 사이드 이펙트가 없는 순수 함수들로 구성될 때 이런 조합성을 만들수 있다
+
+// 순수 함수의 평가 시점을 다루면서 조합성을 다루면서 추상화의 단위를 함수로 하는 프로그래밍
+
+
+
+
+// const sampleList = [
+//   { id: 1, data: 'sample1' },
+//   { id: 2, data: 'sample2' },
+//   { id: 3, data: 'sample3' },
+//   { id: 4, data: 'sample4' },
+// ]
+
+// const _each = (list, iteratee) => {
+// 	for(let i = 0; i < list.length; i++) {
+// 		iteratee(list[i]);
+// 	}
+// }
+
+// const _filter = (list, predicate) => {
+// 	let new_list = [];
+
+// 	_each(list, value => {
+// 		if(predicate(value)) {
+// 			new_list.push(value)
+// 		}
+// 	});
+// }
+
+// const _map = (list, mapper) => {
+//   let new_list = [];
+
+//   _each(list, value => {
+// 		new_list.push(mapper(value));
+// 	})
+
+//   return new_list;
+// }
+
+// const _reduce = (list, iter, memo) => {
+//   if(memo == undefined) {
+//     memo = list[0];
+//     list = _rest(list);
+//   }
+
+//   _each(list, function(val) {
+//     memo = iter(memo, val)
+//   })
+
+//   return memo;
+// }
+
+// const _pipe = (...fns) => arg => _reduce(fns, (arg, fn) => fn(arg), arg);
+
+// const fn = _pipe(
+//   _filter,
+//   _map,
+// )
+
+// fn(sampleList);
