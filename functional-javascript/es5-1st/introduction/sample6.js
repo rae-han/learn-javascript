7ea5ec92064ffafc46a1f31ab262815e6683b48b
Raehan <raehan900809@gmail.com>
2022-04-11 10:49:51 +0900

    modify: function library partial to fxjs

--- functional-javascript/es5-1st/introduction/sample6.js
+++ functional-javascript/es5-1st/introduction/sample6.js
@@ -1,5 +1,5 @@
-var _ = require('fxjs/Strict');
-var L = require('fxjs/Lazy');
+var _ = require('./partial');
+var { L } = _;
 
 // 지연 평가
 //  지연 평가를 시작 시키고 유지 시키는(이어 가는) 함수
@@ -10,25 +10,25 @@ var L = require('fxjs/Lazy');
 // 1. take
 // 2. some, every, find
 
-// var i = 0;
-// var mi = 0;
-// var fi = 0;
+var i = 0;
+var mi = 0;
+var fi = 0;
 
-// _.go(
-//   _.range(100),
-//   _.map(function(val) {
-//     ++i; ++mi;
-//     return val*val;
-//   }),
-//   _.filter(function(val) {
-//     ++i; ++fi;
-//     return val%2;
-//   }),
-//   _.take(5), // 이 전에 배열이 50개다.
-//   console.log,
-// )
+_.go(
+  _.range(100),
+  _.map(function(val) {
+    ++i; ++mi;
+    return val*val;
+  }),
+  _.filter(function(val) {
+    ++i; ++fi;
+    return val%2;
+  }),
+  _.take(5), // 이 전에 배열이 50개다.
+  console.log,
+)
 
-// console.log(i, mi, fi); // filter 함수 전에 mi는 100이다.
+console.log(i, mi, fi); // filter 함수 전에 mi는 100이다.
 
 var i = 0;
 var mi = 0;
