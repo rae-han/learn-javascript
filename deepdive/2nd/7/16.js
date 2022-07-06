101afee4547b77616517bdb4e010fb6fecf0374f
raehan <raehan900809@gmail.com>
2022-07-06 22:15:08 +0900

    add: operator

---
+++ deepdive/2nd/7/16.js
@@ -0,0 +1,4 @@
+console.log(Number.isNaN(0)); // false
+console.log(Number.isNaN(1)); // false
+console.log(Number.isNaN(NaN)); // true
+console.log(Number.isNaN(1+NaN)); // true
---
+++ deepdive/2nd/7/27.js
@@ -0,0 +1,4 @@
+console.log(true && 'here');
+console.log(false && '<- logged false');
+console.log(true || '<- logged true');
+console.log(false || 'here');
