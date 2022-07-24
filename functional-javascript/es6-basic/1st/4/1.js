af4751f1cf3b34fda238469b9b7994df51f2c104
raehan <raehan900809@gmail.com>
2022-07-24 20:06:14 +0900

    add: filter, reduce

--- functional-javascript/es6-basic/1st/4/1.js
+++ functional-javascript/es6-basic/1st/4/1.js
@@ -40,41 +40,39 @@ log(map(p => p.price, products));
 // 함수형 프로그래밍에서는 map 이라는 함수에 보조함수를 통해서, 이터러블 규약을 따르는 어떤 값의 특정 값을 수집하겠다고 전달하는 식으로 사용한다.
 // 추가로 맵 함수는 고차함수이기도 하다. 함수를 값으로 다루면서 내가 원하는 시점에 인자를 적용한다.
 
+// # 이터러블 프로토콜을 따른 map의 다형성
+// - 위에서 구현한 map 함수는 이터러블 프로토콜을 따르고 있기 때문에 다형성이 높다.
 
 
-// // # 이터러블 프로토콜을 따른 map의 다형성
-//
-//
 // log([1, 2, 3].map(a => a + 1));
 //
 // log(map(el => el.nodeName, document.querySelectorAll('*')));
+// dom.__proto__.__proto__.__proto__.__proto__.__proto__.__proto__
 //
-// // const it = document.querySelectorAll('*')[Symbol.iterator]();
+// // const it = document.querySelectorAll('*')[Symbol.iterator](); // 이터러블을 따른다
 // // log(it.next());
 // // log(it.next());
 // // log(it.next());
 // // log(it.next());
 // // log(it.next());
-//
-// function* gen() {
-//   yield 2;
-//   if (false) yield 3;
-//   yield 4;
-// }
-//
-// log(map(a => a * a, gen()));
-//
-// let m = new Map();
-// m.set('a', 10);
-// m.set('b', 20);
-// log(new Map(map(([k, a]) => [k, a * 2], m)));
-//
-//
-//
-//
-// // # filter
-//
-//
+
+function* gen() {
+  yield 2;
+  if (false) yield 3;
+  yield 4;
+}
+
+log(map(a => a * a, gen())); // 이것도 된다 다형성이 높다.
+// 이터러블 프로토콜을 따르는 함수들을 사용하는 것은
+// 앞으로 많은 함수들과의 조합성이 좋아진다는 이야기이다.
+
+let m = new Map();
+m.set('a', 10);
+m.set('b', 20);
+log(new Map(map(([k, a]) => [k, a * 2], m)));
+
+// # filter // 걸러내는 코드
+
 // const filter = (f, iter) => {
 //   let res = [];
 //   for (const a of iter) {
@@ -111,8 +109,9 @@ log(map(p => p.price, products));
 //
 //
 //
-// // # reduce
-//
+// # reduce
+// 값을 축약하는 함수
+// 다른 값을 하나의 값으로 축약해 나가는 함수
 //
 // const nums = [1, 2, 3, 4, 5];
 //
@@ -138,7 +137,7 @@ log(map(p => p.price, products));
 // log(reduce(add, 0, [1, 2, 3, 4, 5]));
 // // 15
 //
-// log(add(add(add(add(add(0, 1), 2), 3), 4), 5));
+// log(add(add(add(add(add(0, 1), 2), 3), 4), 5)); 결국 원리는 이거다. 연속적 재귀적으로 함수를 연속적으로 실행하도록 구현해야한다.
 // // 15
 //
 // log(reduce(add, [1, 2, 3, 4, 5]));
@@ -149,6 +148,7 @@ log(map(p => p.price, products));
 //
 //   log(
 //   reduce(
-//   (total_price, product) => total_price + product.price,
+//   (total_price, product) => total_price + product.price, // 리듀서 역시 보조함수로 안쪽에 있는 값의 다형성을 잘 지원해준다?
 //   0,
-//   products));
+//   products)); // 이터러블을 통해서 외부 다형성도 잘 지원해준다
+//
